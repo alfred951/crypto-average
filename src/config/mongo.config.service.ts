47e60caf159c2e7c031517064d7bd614291252cb
Luis Gallego <alfredogallegom@gmail.com>
2021-10-12 16:40:55 -0500

    feat: adding typeORM and Environment configuration

---
+++ src/config/mongo.config.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class MongoConfigService {
+  constructor(private readonly config: ConfigService) {}
+
+  getConnectionConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'mongodb',
+      useUnifiedTopology: true,
+      autoLoadEntities: true,
+      database: 'crypto',
+      authSource: 'admin',
+      synchronize: true,
+      host: this.config.get<string>('MONGODB_HOST'),
+      port: this.config.get<number>('MONGODB_PORT'),
+      username: this.config.get<string>('MONGODB_USERNAME'),
+      password: this.config.get<string>('MONGODB_PASSWORD'),
+    };
+  }
+}
